434d229290313eacdc413e3760c3fb436de469e6
Orlando <orlando.boffill@gmx.com>
2015-10-09 11:17:29 +1100

    dec2hex

--- 3/3.8.dec2hex.cpp
+++ 3/3.8.dec2hex.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <iomanip>
+#include <cmath>
 using namespace std;
 
 string GetLine() {
@@ -27,19 +29,34 @@ int GetInteger() {
 }
 
 bool HasHexLetters(int myInt) {
-    if(myInt%16 > 9)
-        return true;
-    else
-        return false;           
+    bool HasHex = false;
+    int pow16;
+    int result = myInt;
+    for(int i=0; pow(16, i) <= myInt; ++i) {
+        pow16 = i;
+    }
+    for(int i=pow16; myInt > 0; i--) {
+        cout << "myInt: " << myInt << endl;
+        int coeff = (int)(myInt/pow(16,i));
+        cout << "coeff: " << myInt << "/" << "pow(16, " << i << ")" << " = " << coeff << endl;
+        if(coeff>9) return HasHex = true;
+        cout << "delta: " << myInt;
+        myInt = myInt - (coeff*pow(16,i));
+        cout << " - (" << coeff << "*" << pow(16,i) << ") = " << myInt << endl;
+
+    }
+
+    return HasHex;
 }
 
 int main() {
     while(true) {
+        cout <<  "Enter a number: ";
         int myInt = GetInteger();
-        cout << "Dec: " << dec << myInt << endl;
-        cout << "Hex: " << hex << myInt << endl;
-        cout << "Mod16: " << dec << (myInt%16) << endl;
-        cout << "Has alpha char? " << boolalpha << HasHexLetters(myInt) << endl;
+        cout << setw(10) << "Alpha:" << setw(10) <<  boolalpha << HasHexLetters(myInt) << endl;
+        cout << setw(10) << "Dec:" << setw(10) << dec << myInt << dec << endl;
+        cout << setw(10) << "Hex:" << setw(10) << hex << myInt << dec << endl;
+
     }
     return 0;
 }
